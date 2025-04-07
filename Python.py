18afe5df82708a1a33350c8c22fbd699355f408b
Snehalchavan12 <sneh.more88@gmail.com>
2025-04-07 16:37:21 +0530

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,14 @@
+a = 12
+b = 6
+
+# addition
+print ('sum: ', a + b)
+
+# subtraction
+print ('subtraction: ', a - b)
+
+# multiplication
+print ('multiplication: ', a * b)
+
+# division
+print ('division: ', a / b)
